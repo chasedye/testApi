7452e5d5148cf378ce8488e131acf07df9344604
Juan Guarnizo <jdavid29@knights.ucf.edu>
2022-07-25 16:59:48 -0400

    Minor changes to homepage.js

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,7 +1,5 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const Recipe = require("../models/recipes");
 const unirest = require("unirest");
 
 const apiKey = '0a6685e6362045c38a6ca038dd3431c8'
@@ -14,14 +12,13 @@ router.get('/findByIngredients', (req, res) => {
   //body of request will  be a json containing the array of ingredients
   const ingredients = req.body.ingredients;
 
-  console.log(`the array of ingredients read is: ${ingredients}`);
-
   //create a string with the ingredients
   var string = `${ingredients}`;
+  //edit string to comply with format required by Spoonacular API
   string = string.split(',').join(",+");
-  console.log(`the string modified is: ${string}`);
+  //concatenate strings
   serviceURL = serviceURL + string;
-  console.log(`the service url is now: ${serviceURL}` );
+
 
   //use spoonacular API
   //send the response back to the client
