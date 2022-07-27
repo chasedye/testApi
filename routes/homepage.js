fbb3436ec066f377e96b40fa4b56df60582e0723
Juan Guarnizo <jdavid29@knights.ucf.edu>
2022-07-26 22:56:49 -0400

    Added new feature

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,38 +2,34 @@ const express = require("express");
 const router = express.Router();
 const unirest = require("unirest");
 
-const apiKey = '0a6685e6362045c38a6ca038dd3431c8'
+const apiKey = "0a6685e6362045c38a6ca038dd3431c8";
 //search recipe given the ingredients
 
-router.get('/findByIngredients', (req, res) => {
+router.get("/findByIngredients", (req, res) => {
   //service url will be used to call the spoonacular API
   let serviceURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=`;
-  
+
   //body of request will  be a json containing the array of ingredients
   const ingredients = req.body.ingredients;
 
   //create a string with the ingredients
   var string = `${ingredients}`;
   //edit string to comply with format required by Spoonacular API
-  string = string.split(',').join(",+");
+  string = string.split(",").join(",+");
   //concatenate strings
   serviceURL = serviceURL + string;
 
-
   //use spoonacular API
   //send the response back to the client
-  unirest.get(serviceURL).end(result => {
-    if(result.status == 200){
-      res.send(result.body);
+  unirest.get(serviceURL).end((result) => {
+    if (result.status == 200) {
+      res.json(result.body);
       res.status(200);
+    } else {
+      res.send("Something failed \n");
     }
-
-    else{
-      res.send('Something failed \n');
-    }
-  })
-  
-})
+  });
+});
 //successfully tested on postman
 
 module.exports = router;
